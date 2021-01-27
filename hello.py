af24a03de01c1899948a89afeeebe016f388280e
AndrewKichakov <ku4akov@gmail.com>
2021-01-27 19:22:32 +0300

    Second commit

--- hello.py
+++ hello.py
@@ -1,10 +1,8 @@
 def app(environ, start_response):
-    status = "200 OK"
+    status = '200 OK'
     headers = [
-        ("Content-Type", "text/plain")
+        ('Content-Type', 'text/plain')
     ]
-    body = []
-    for k, v in environ.items():
-        body.append(f"{k}={v}")    
+    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]   
     start_response(status, headers)
     return body
