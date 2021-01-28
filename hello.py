4990462398fe24b8d44ee67d978f348a4a22306f
AndrewKichakov <ku4akov@gmail.com>
2021-01-28 15:30:18 +0300

    New upgrade

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
 def app(environ, start_response):
+    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
     status = '200 OK'
     headers = [
-        ('Content-Type', 'text/plain')
-    ]
-    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]   
+        ('Content-Type', 'text/plain'),
+        ('Content-Length', str(len(body)))
+    ]  
     start_response(status, headers)
     return body
